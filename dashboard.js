c311e876d7b8671d55d838ef49498270396bba05
antoniaalbu <antoniaxv@gmail.com>
2024-10-31 11:08:31 +0200

    Updated Layout

--- dashboard.js
+++ dashboard.js
@@ -73,6 +73,10 @@ async function fetchNews() {
     }
 }
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 function displayNews(articles) {
     const newsContainer = document.getElementById("news-info");
     newsContainer.innerHTML = ""; 
@@ -101,11 +105,22 @@ function displayNews(articles) {
 
         newsContainer.appendChild(newsItem);
     });
+<<<<<<< Updated upstream
 }
 
 fetchNews();
 
 // Quote
+=======
+
+  
+    newsContainer.scrollTop = 0;
+}
+
+
+fetchNews();
+
+>>>>>>> Stashed changes
 
 document.addEventListener("DOMContentLoaded", () => {
     const quoteText = document.querySelector(".quote-text"), 
@@ -135,11 +150,57 @@ document.addEventListener("DOMContentLoaded", () => {
     quoteBtn.addEventListener("click", randomQuote);
 });
 
+<<<<<<< Updated upstream
 
 
 
 
 
 
+=======
+const scrollingText = document.getElementById("scrolling-text");
+
+const API_URL = 'https://api.exchangerate-api.com/v4/latest/';
+const displayedCurrencies = ["EUR", "GBP", "USD", "JPY", "CAD"];
+
+
+async function fetchExchangeRates() {
+    try {
+        const response = await fetch(`${API_URL}RON`); 
+        const data = await response.json();
+
+        startScrollingRates(data.rates);
+    } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+        alert('Failed to load exchange rates from API. Using local data if available.');
+
+        const savedRates = localStorage.getItem('exchangeRates');
+        if (savedRates) {
+            const rates = JSON.parse(savedRates);
+            startScrollingRates(rates);
+        } else {
+            alert('No local data available.');
+        }
+    }
+}
+
+
+function startScrollingRates(rates) {
+    const exchangeRatesList = displayedCurrencies.map(currency => {
+        return `${currency}: ${rates[currency] ? rates[currency].toFixed(2) : 'N/A'}`;
+    });
+
+    let index = 0;
+    setInterval(() => {
+        scrollingText.textContent = `Current Exchange Rates - ${exchangeRatesList[index]}`;
+        index = (index + 1) % exchangeRatesList.length;
+    }, 3000);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchExchangeRates(); 
+});
+>>>>>>> Stashed changes
 
 
