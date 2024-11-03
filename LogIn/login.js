a19b074911cea3f4a22307410fc1634459a80486
Francesco Maxim <francesco.maxim@icloud.com>
2024-11-04 01:14:03 +0200

    verify fields

--- LogIn/login.js
+++ LogIn/login.js
@@ -6,23 +6,56 @@ const myButton = document.getElementById("myButton");
 counter = 0;
 
 myButton.addEventListener("click", () => {
-  counter++;
+  resetError();
   if (counter === 0) {
+    if (myInput.value.length <= 2) {
+      errorMessage("username");
+    } else {
+      goToEmail();
+      counter++;
+    }
   } else {
     if (counter === 1) {
-      myHeader.innerHTML = `What's your email, ${myInput.value}?`;
-      myInput.value = "";
-      mySmall.innerHTML = "Enter your emal to create an account or log in";
+      isValid = validateEmail(myInput.value);
+      if (isValid) {
+        goToPassword();
+        counter++;
+      } else {
+        errorMessage("email");
+      }
     } else {
       if (counter === 2) {
-        myHeader.innerHTML = `Enter your password`;
-        myInput.setAttribute("type", "password");
-        myInput.value = "";
-        mySmall.innerHTML = "";
+        if (myInput.value.length < 4) {
+          errorMessage("password");
+        } else {
+          //successfull
+        }
       }
     }
   }
 });
+
+function goToPassword() {
+  myHeader.innerHTML = `Enter your password`;
+  myInput.setAttribute("type", "password");
+  myInput.value = "";
+  mySmall.innerHTML = "";
+}
+
+function goToEmail() {
+  myHeader.innerHTML = `What's your email, ${myInput.value}?`;
+  myInput.value = "";
+  mySmall.innerHTML = "Enter your emal to create an account or log in";
+}
+
+function errorMessage(message) {
+  mySmall.innerHTML = `Please enter a valid ${message}!`;
+  mySmall.style.color = "red";
+}
+
+function resetError() {
+  mySmall.style.color = "white";
+}
 
 function validateEmail(email) {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
