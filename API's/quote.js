bd8f1d1759965b4e70af3e9611727dae6e8e0ba6
Francesco Maxim <francesco.maxim@icloud.com>
2024-11-04 04:39:10 +0200

    fixed quote feature

--- API's/quote.js
+++ API's/quote.js
@@ -1,13 +1,13 @@
 const quote = document.getElementById("mySmall");
+const author = document.getElementById("mySmall2");
 
 async function updateQuote() {
   // Fetch a random quote from the Quotable API
   const response = await fetch("https://api.quotable.io/random");
   const data = await response.json();
   if (response.ok) {
-    // Update DOM elements
     quote.textContent = data.content;
-    // cite.textContent = data.author;
+    author.textContent = "-" + data.author;
   } else {
     quote.textContent = "An error occured";
     console.log(data);
