20262dce63dfbabb8b7f30b74c8a85dce3e2cc3e
Diana <dianamariapetrula@gmail.com>
2024-11-06 21:27:15 +0200

    added a max quote length

--- API's/quote.js
+++ API's/quote.js
@@ -2,16 +2,27 @@ const quote = document.getElementById("mySmall");
 const author = document.getElementById("mySmall2");
 
 async function updateQuote() {
-  // Fetch a random quote from the Quotable API
-  const response = await fetch("https://api.quotable.io/random");
-  const data = await response.json();
-  if (response.ok) {
-    quote.textContent = data.content;
-    author.textContent = "-" + data.author;
-  } else {
-    quote.textContent = "An error occured";
-    console.log(data);
-  }
+    const maxQuoteLength = 150;
+    let quoteText = "";
+    let authorText = "";
+
+    while (!quoteText || quoteText.length > maxQuoteLength) {
+        // Fetch a random quote from the Quotable API
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        if (response.ok) {
+            quoteText = data.content;
+            authorText = data.author;
+        } else {
+            quote.textContent = "An error occurred";
+            console.log(data);
+            break; 
+        }
+    }
+
+    // Display the quote if it meets the length requirement
+    quote.textContent = quoteText;
+    author.textContent = "-" + authorText;
 }
 
 updateQuote();
