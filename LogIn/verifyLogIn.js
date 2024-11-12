09d93e4ce79e4b6d7a789c2b640e4f0918bb3708
Francesco Maxim <francesco.maxim@icloud.com>
2024-11-13 01:13:38 +0200

    removed unnecesary files

--- LogIn/verifyLogIn.js
+++
@@ -1,8 +0,0 @@
-// checkLogIn();
-
-// function checkLogIn() {
-//   let key = localStorage.getItem("user");
-//   if (key == null) {
-//     window.location.href = "LogIn/login.html";
-//   }
-// }
